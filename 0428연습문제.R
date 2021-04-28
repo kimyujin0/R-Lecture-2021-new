e4c6fb527e6967afa5b5d8c58e982a301c2557d7
kimyujin0 <kyujin9691@gmail.com>
2021-04-28 17:00:20 +0900

    연습문제

--- 0428연습문제.R
+++ 0428연습문제.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+
 mpg <- as.data.frame(ggplot2::mpg)
 # 1. mpg 데이터의 cty(도시 연비)와 hwy(고속도로 연비) 간에 어떤 관계가 있는지 알아보려고 합니다. x축은 cty, y축은 hwy로 된 산점도를 만들어 보세요.
 mpg %>% ggplot(aes(cty, hwy)) + geom_point()
